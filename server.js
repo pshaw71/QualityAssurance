bf7552d19757416056875e0579c8027701156ca3
Shaw <99703081+pshaw71@users.noreply.github.com>
2023-04-03 12:06:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
